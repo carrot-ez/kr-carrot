22f974620feb992c2342eeef8ce37fd803cca19a
CarrotNuts <kain6245@gmail.com>
2021-07-22 17:17:38 +0900

    add global CORS config

---
+++ Spring/src/main/java/kr/carrot/Spring/config/WebConfig.java
@@ -0,0 +1,18 @@
+package kr.carrot.Spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    // CORS
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://kr-cat.web.app")
+                .allowedOriginPatterns("http://localhost*")
+                .allowedMethods("GET", "POST");
+    }
+}
