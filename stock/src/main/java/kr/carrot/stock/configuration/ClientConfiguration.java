c0ad5d310f13be167e7995c8774763d1f20b5a15
carrot-ez <kain6245@gmail.com>
2023-11-02 11:35:48 +0900

    fix: RestTemplate Configuration

--- stock/src/main/java/kr/carrot/stock/configuration/ClientConfiguration.java
+++ stock/src/main/java/kr/carrot/stock/configuration/ClientConfiguration.java
@@ -1,10 +1,11 @@
 package kr.carrot.stock.configuration;
 
 import kr.carrot.core.configuration.DataGovConfiguration;
+import kr.carrot.core.configuration.RestTemplateConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import(DataGovConfiguration.class)
+@Import({DataGovConfiguration.class, RestTemplateConfiguration.class})
 public class ClientConfiguration {
 }
